b982bb18afd60ccb812ca0714189a8ff2ef48f9c
ShrutheeshIR <shrutheesh99@gmail.com>
2022-06-04 18:25:36 -0700

    changed and added shell scripts for easier interface with desktop GUI. Also added image_view package for realsense

--- realsense_capture/build_ros.sh
+++ realsense_capture/build_ros.sh
@@ -1,7 +1,7 @@
-cd catkin_ws
+cd /maya-slam/realsense_capture/catkin_ws
 export ROS_VERSION=melodic
 catkin build -j$(($(nproc)-1))
 
 cd ..
 chmod +x start_camera.sh
-chmod +x start_recording.sh
+chmod +x stop_camera.sh
---
+++ slam_algorithms/maplab/stop_maplab.sh
@@ -0,0 +1,3 @@
+cd /maya-slam/slam_algorithms/catkin_ws
+source devel/setup.bash
+rosnode kill -a
