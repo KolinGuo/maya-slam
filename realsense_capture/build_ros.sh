391f8265b29bbe8dd5428e9309cba4aaabc73cb4
Kolin Guo <ruguo@ucsd.edu>
2022-06-06 22:40:48 -0700

    Update build_ros.sh

--- realsense_capture/build_ros.sh
+++ realsense_capture/build_ros.sh
@@ -1,5 +1,7 @@
 cd /maya-slam/realsense_capture/catkin_ws
 export ROS_VERSION=melodic
+catkin init
+catkin config --extend /opt/ros/"$ROS_VERSION"
 catkin build -j$(($(nproc)-1))
 
 cd ..
--- slam_algorithms/maplab/build_ros.sh
+++ slam_algorithms/maplab/build_ros.sh
@@ -2,7 +2,7 @@ cd /maya-slam/slam_algorithms/maplab/catkin_ws
 export ROS_VERSION=melodic
 catkin init
 catkin config --merge-devel # Necessary for catkin_tools >= 0.4.
-catkin config --extend /opt/ros/$ROS_VERSION
+catkin config --extend /opt/ros/"$ROS_VERSION"
 catkin config --cmake-args -DCMAKE_BUILD_TYPE=Release
 
 catkin build yaml_cpp_catkin -j$(($(nproc)-1))
