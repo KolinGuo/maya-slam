248b0c15741b97bd5228976fbcb837dc0fbf4405
Kolin Guo <ruguo@ucsd.edu>
2022-06-09 19:33:53 -0700

    [Bug fix] Fix the error resulting from not running pose refine from GUI

--- scene_recon/open3d_tsdf/build_mesh.sh
+++ scene_recon/open3d_tsdf/build_mesh.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 cd /maya-slam/scene_recon/open3d_tsdf
 source /opt/ros/noetic/setup.bash
-python3 open3d_tsdf_reconstruction.py /maya-slam/slam_algorithms/rosbags/"$1" --voxel-length "$2" "$3"
+if [ -z "$3" ] ; then
+  python3 open3d_tsdf_reconstruction.py /maya-slam/slam_algorithms/rosbags/"$1" --voxel-length "$2"
+else
+  python3 open3d_tsdf_reconstruction.py /maya-slam/slam_algorithms/rosbags/"$1" --voxel-length "$2" "$3"
+fi
